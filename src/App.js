3fe15a1657ac42ea4cceb7695024b7b11fb7a634
Gregor Zunic <greg@GregorsMacBookPro.Home>
2019-06-26 22:42:20 +0200

    konc za dans

--- src/App.js
+++ src/App.js
@@ -24,7 +24,8 @@ class App extends React.Component {
 				metadata: {}
 			},
 			isLoaded: false,
-			showRecommendations: false
+			showRecommendations: false,
+			ShowNrOfSearches: false
 		};
 	}
 
@@ -39,7 +40,8 @@ class App extends React.Component {
 							query: json.query,
 							rec_materials: json.rec_materials,
 							metadata: json.metadata,
-							showRecommendations: false
+							showRecommendations: false,
+							ShowNrOfSearches: true
 						}
 					});
 				});
@@ -65,7 +67,12 @@ class App extends React.Component {
 				.filter(word => word.startsWith(this.state.search_key))
 				.map(item => (
 					<li>
-						<button onClick={this.AcceptRec.bind(this, item)}>{item}</button>
+						<button
+							className="btn bg-transparent"
+							onClick={this.AcceptRec.bind(this, item)}
+						>
+							{item}
+						</button>
 					</li>
 				))
 				.slice(0, 6);
@@ -77,22 +84,49 @@ class App extends React.Component {
 		this.setState({ search_key: name, showRecommendations: false });
 	};
 
+	NrOfSearches = () => {
+		if (this.state.ShowNrOfSearches === true)
+			return (
+				<p>
+					Number of search results found:{' '}
+					{this.state.api_search.metadata.num_or_materials} for{' '}
+					{this.state.api_search.query.text}
+				</p>
+			);
+		else {
+			return null;
+		}
+	};
 	SearchItem = item => {
 		let sitem = item.item;
-		if (sitem.description) {
-			sitem.description = sitem.description.substr(0, 200) + ' ...';
+		if (sitem.description && sitem.description.length > 280) {
+			sitem.description = sitem.description.substr(0, 280) + ' ...';
 		}
 		return (
 			<li>
-				<h1>{sitem.title}</h1>
-				<p>{sitem.description}</p>
+				<div className="jumbotron bg-transparent py-1 my-1">
+					<a href={sitem.url} target="blank">
+						<h4 className="searched">{sitem.title}</h4>
+					</a>
+					<span className="text-muted">
+						<small>{sitem.url}</small>
+					</span>
+					<p>{sitem.description}</p>
+				</div>
 			</li>
 		);
 	};
 
 	render() {
 		return (
-			<div className="main">
+			<div className="container">
+				<div className="text-center">
+					<img
+						src="https://www.x5gon.org/wp-content/themes/x5gon/dist/assets/img/logo.svg"
+						alt="X5GON"
+						className="my-5 img-fluid"
+					/>
+				</div>
 				<input
 					ref={input => input && input.focus()}
 					type="text"
@@ -100,17 +134,22 @@ class App extends React.Component {
 					id={'todoName' + this.props.id}
 					onChange={e => this.ChangeSearchKey(e.target.value)}
 					onKeyDown={this.CheckEnter}
+					placeholder="Search for OER material"
+					className="form-control align-middle mb-3"
+					autoComplete="off"
 				/>
-				<button onClick={this.searchComponent.bind(this)}>press</button>
-				<ul>
+				<button
+					type="button"
+					className="btn btn-outline-primary"
+					onClick={this.searchComponent.bind(this)}
+				>
+					search
+				</button>
+				<ul className="recommendations">
 					<this.Recommendations />
 				</ul>
-				<p>
-					Number of search results found:{' '}
-					{this.state.api_search.metadata.num_or_materials} for{' '}
-					{this.state.api_search.query.text}
-				</p>
-				<ul>
+				<this.NrOfSearches />
+				<ul className="searched-items">
 					{this.state.api_search.rec_materials.map(item => (
 						<this.SearchItem item={item} />
 					))}
