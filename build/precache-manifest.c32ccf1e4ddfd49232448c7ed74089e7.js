1d438713e5804b73d34b68889a0a8f91d341bdc7
Gregor Zunic <greg@GregorsMacBookPro.Home>
2019-06-28 14:57:50 +0200

    fix?

---
+++ build/precache-manifest.c32ccf1e4ddfd49232448c7ed74089e7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a15264001e36b0bc79fbdd7a7970797d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "8a77c18dfdbb41186daf",
+    "url": "./static/css/main.5b4d68f6.chunk.css"
+  },
+  {
+    "revision": "24a4d03b83978f20eb19",
+    "url": "./static/js/2.d149b834.chunk.js"
+  },
+  {
+    "revision": "8a77c18dfdbb41186daf",
+    "url": "./static/js/main.6d7863a3.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
